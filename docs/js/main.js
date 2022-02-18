3c0721f89f1975e4036af97487e7487a500878d1
natistuomi <77719270+natistuomi@users.noreply.github.com>
2022-02-18 12:17:59 +0100

    fail

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,11 @@
-console.log("loaded");
+let aktivitet_knapp = "";
+let amount_knapp = "";
+let type_knapp = "";
+let date_knapp = "";
+let time_knapp = "";
+
+function aktivitet(x){
+    aktivitet_knapp = x;
+    document.getElementById("boka_aktivitet").style.display = "none";
+    document.getElementById("boka_platser").style.display = "block";
+}
